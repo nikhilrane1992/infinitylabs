fb2bb57e7faa8914dcc7eea96ebb2a91041669e4
Nikhil Rane <nikhilrane1992@gmail.com>
2017-10-25 17:41:33 +0530

    -Added router details and tenant models

---
+++ infinitylabs/infinitylabs/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class RouterDetails(models.Model):
+    loopback = models.CharField(max_length=100)
+    hostname = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    item_height = models.CharField(max_length=100)
+    item_weight = models.CharField(max_length=100)
+    dimensions = models.CharField(max_length=100)
+    model_number = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "{}".format(self.title)
+
+    class Meta:
+        unique_together = ("loopback", "hostname")
+
+
+class Tenant(models.Model):
+    name = models.CharField(max_length=50)
+    api_key = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "Name: {} | Api Key: {}".format(self.name, self.api_key)
